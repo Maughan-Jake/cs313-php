b06eb8c348d9cc20c8733722fe5db80b9e9fba93
Jake Maughan <jacobm2n527@gmail.com>
2019-09-24 15:16:59 -0500

    finished basics

--- web/week2/main.js
+++ web/week2/main.js
@@ -6,6 +6,8 @@ function changeBgColor() {
     document.getElementById("box1").style.background = document.getElementById("color").value;
 }
 
-// $(document).ready(function(){
-//     $('#box1').css('background-color', 'red')
+// $(function(){
+//   $("#changeColor").click(function() {
+//     $('#box1').css('background-color', 'red');
+//   })
 // });

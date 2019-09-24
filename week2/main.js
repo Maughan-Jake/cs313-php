c1ab8e7bde8ae3a4ae984d790c392072e550e502
Jacob Maughan <jacobm.2n527@gmail.com>
2019-09-24 11:31:20 -0400

    created js file for week 2

---
+++ week2/main.js
@@ -0,0 +1,11 @@
+function alertMe() {
+    alert("Clicked!");
+  }
+
+function changeBgColor() {
+    document.getElementById("box1").style.background = document.getElementById("color").value;
+}
+
+// $(document).ready(function(){
+//     $('#box1').css('background-color', 'red')
+// });

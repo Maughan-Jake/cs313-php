71c2e45b53a7073bbe07a2cee52a9b219aa37866
Jake Maughan <jacobm2n527@gmail.com>
2019-09-24 15:24:29 -0500

    added week 2 team activity

---
+++ web/week2/main.js
@@ -0,0 +1,13 @@
+function alertMe() {
+    alert("Clicked!");
+  }
+
+function changeBgColor() {
+    document.getElementById("box1").style.background = document.getElementById("color").value;
+}
+
+// $(function(){
+//   $("#changeColor").click(function() {
+//     $('#box1').css('background-color', 'red');
+//   })
+// });

5e443e81f0a1af455d525b1cdfae267f4c84b5c5
Jake Maughan <jacobm2n527@gmail.com>
2019-10-19 19:57:22 -0600

    updated

--- web/week5/find-items.php
+++ web/week5/find-items.php
@@ -12,7 +12,7 @@ $db = dbConnect();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>JakesList - Search</title>
-  <link rel="stylesheet" media="screen" href="main.css" type="html/css">
+  <link rel="stylesheet" media="screen" href="main.css" type="text/css">
 </head>
 <body>
   <header>

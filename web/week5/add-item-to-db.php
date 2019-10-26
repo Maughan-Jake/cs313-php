c25f4343c4687976389078c6661e78bb5623b136
Jake Maughan <jacobm2n527@gmail.com>
2019-10-26 11:02:30 -0600

    updated

--- web/week5/add-item-to-db.php
+++ web/week5/add-item-to-db.php
@@ -5,42 +5,10 @@ $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_STRING);
 $price = filter_input(INPUT_POST, 'price', FILTER_SANITIZE_NUMBER_FLOAT);
 $category = filter_input(INPUT_POST, 'category', FILTER_SANITIZE_STRING);
 
-// if (
-//     empty($name) || empty($description) || empty($price) || empty($category)
-// ) {
-//     $message = '<p>Please fill all empty form fields</p>';
-//     include 'index.php';
-//     exit;
-// }
-
 if (
-    empty($name) 
+    empty($name) || empty($description) || empty($price) || empty($category)
 ) {
-    $message = '<p>Please fill in the name field</p>';
-    include 'index.php';
-    exit;
-}
-
-if (
-    empty($description)
-) {
-    $message = '<p>Please fill in the description field</p>';
-    include 'index.php';
-    exit;
-}
-
-if (
-    empty($price)
-) {
-    $message = '<p>Please fill in the price field</p>';
-    include 'index.php';
-    exit;
-}
-
-if (
-    empty($category)
-) {
-    $message = '<p>Please fill in the category</p>';
+    $message = '<p>Please fill all empty form fields</p>';
     include 'index.php';
     exit;
 }
@@ -48,8 +16,8 @@ if (
 require "db-connect.php";
 $db = dbConnect();
 
-$sql = 'INSERT INTO inventory (name, description, price, category_id) 
-        VALUES(:name, :description, :price, :categoryID)';
+$sql = 'INSERT INTO inventory (name, description, price, customer_id, category_id) 
+        VALUES(:name, :description, :price, 1, :categoryID)';
     $stmt = $db->prepare($sql);
     
     $stmt->bindValue(':name', $name. PDO::PARAM_STR);

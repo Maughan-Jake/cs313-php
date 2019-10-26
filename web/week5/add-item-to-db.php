8985bbfc4ac92690d8e0ef0d203333151382ad3c
Jake Maughan <jacobm2n527@gmail.com>
2019-10-26 08:58:18 -0600

    updated

--- web/week5/add-item-to-db.php
+++ web/week5/add-item-to-db.php
@@ -1,6 +1,6 @@
 <?php
 
-$name= $_POST['itemName'];
+$name = $_POST['itemName'];
 $description = $_POST['description'];
 $price = $_POST['price'];
 $category = $_POST['value'];
@@ -8,7 +8,8 @@ $category = $_POST['value'];
 require "db-connect.php";
 $db = dbConnect();
 
-$query = 'INSERT INTO inventory(name, description, price, category_id) VALUES(:name, :description, :price, :categoryID)';
+$query = 'INSERT INTO inventory (name, description, price, category_id) 
+          VALUES(:name, :description, :price, :categoryID)';
     $statement = $db->prepare($query);
     
     $statement->bindValue(':name', $name);
@@ -17,7 +18,9 @@ $query = 'INSERT INTO inventory(name, description, price, category_id) VALUES(:n
     $statement->bindValue(':categoryID', $category);
     
     $statement->execute();
-
+    $stmt->closeCursor();
+    // Return the indication of success (rows changed)
+    return $rowsChanged;
     include 'index.php';
     die();
 

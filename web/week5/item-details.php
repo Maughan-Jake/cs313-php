9f153b12ff020c490623a91048704c7ac4ad6864
Jake Maughan <jacobm2n527@gmail.com>
2019-10-26 10:05:39 -0600

    updated

--- web/week5/item-details.php
+++ web/week5/item-details.php
@@ -43,6 +43,7 @@ $db = dbConnect();
                 echo '<h3 class="title">' . $row['name'] . '</h3>';
                 echo '<div id="description-text">';
                 echo '<p class="description">' . $row['description'] . '</p>';
+                echo '<p>' . $row['first_name'] . ' ' . $row['last_name'] . ' ' . $row['email'] . '</p>';
                 echo '</div>';
                 echo '<p class="price-box">' . $row['price']  . '</p>';
                 // join the tables so I can also echo the seller name and email as contact info

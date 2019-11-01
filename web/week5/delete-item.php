35953578b18aaf6327fdb8e34fc6d03450e0138a
Jake Maughan <jacobm2n527@gmail.com>
2019-11-01 16:41:45 -0600

    updated

--- web/week5/delete-item.php
+++ web/week5/delete-item.php
@@ -26,7 +26,7 @@
     <h1>Update Item in Jakeslist</h1>
 
     <form action="update.php" method="post"> 
-        <h2>Are you sure you want to delete this item?</h2>
+        <h2 class="del-text">Are you sure you want to delete this item?</h2>
 
             <input type="submit" name="action" value="Delete">
             <input type="hidden" name="id" value="<?php echo ($_GET['id']) ?>">
